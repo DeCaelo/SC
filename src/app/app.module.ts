51d32de6f6351d4afdc2e5e42ca4993aaf8d5453
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-10 17:46:42 +0100

    routing for market & collection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MarketComponent } from './market/market.component';
 import { CollectionComponent } from './collection/collection.component';
 import { CollectableService } from "./shared/collectable.service";
+import { HeaderComponent } from './header.component';
+import { routing } from "./app.routing";
 
 @NgModule({
   declarations: [
     AppComponent,
     MarketComponent,
-    CollectionComponent
+    CollectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    routing
   ],
   providers: [CollectableService],
   bootstrap: [AppComponent]
