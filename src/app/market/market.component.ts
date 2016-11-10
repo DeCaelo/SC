9f02706d08cf4be62b97d52381d824cc7270779a
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-10 15:53:27 +0100

    list & loop for collectables items

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class MarketComponent implements OnInit {
+  collectables = [
+    { description: 'A very rare copy of "jQuery for Dummies"', type: 'Book' },
+    { description: 'The first Letter ever written', type: 'Piece of Paper' },
+    { description: 'A photograph showing nothing', type: 'Photo' },
+    { description: 'A box with all sold Zunes', type: 'Garbage' }
+  ];
 
   constructor() { }
 
